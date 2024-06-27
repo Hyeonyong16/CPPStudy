7f800f246cd583d15ca1c6b1eac1acd387c7f0cd
Hyeonyong16 <49775204+Hyeonyong16@users.noreply.github.com>
2024-06-28 00:01:56 +0900

    지뢰찾기2
    지뢰 보드 배열 오류 수정
    지뢰 찾기 기능 추가
     - 맵과 스크린을 통한 표시
     - 0일 경우 연쇄적으로 열리는 기능 구현 (재귀함수)
    
    다음에 구현할 것
     - 기능 함수로 구현
     - 구현한 함수 파일 분할하기

--- CPP_Study03/main01(재귀함수).cpp
+++ CPP_Study03/main01(재귀함수).cpp
@@ -6,39 +6,144 @@
 #include <stdlib.h>
 #include <time.h>
 
+//board에서 입력받은 x, y좌표를 확인하는 함수
+bool Dig(int _x, int _y, char _board[][10], char _screen[][10])
+{
+	//좌표를 1~10으로 입력 받기에 1씩 빼줌
+	int X = _x - 1;
+	int Y = _y - 1;
+
+	bool val = true;	//스크린에 반환할 값
+
+
+	if (_board[Y][X] == '*') return false;	//지뢰를 누르면 false 반환
+	if (X < 0 || X > 9) return false;
+	if (Y < 0 || Y > 9) return false;
+
+	if (_screen[Y][X] != '+') return true;	//이미 확인한 칸이면 0반환
+	
+	if (val) _screen[Y][X] = _board[Y][X];
+
+	for (int h = -1; h < 2; h++)
+	{
+		for (int w = -1; w < 2; w++)
+		{
+			if(_screen[Y][X] == '0') val = Dig(_x-w, _y-h, _board, _screen);
+		}
+	}
+
+	return true;
+}
+
 int main()
 {
-	int board[10][10] = { 0 };	//10 * 10 크기 보드 생성 및 초기화
-	int numBombs = 15;			//지뢰의 개수
+	char board[10][10] = { 0 };	//10 * 10 크기 보드 생성 및 초기화
+	char screen[10][10] = { 0 };	//화면에 출력할 스크린
+	int numBombs = 0;			//지뢰의 개수
 	int numBombsOnBoard = 0;	//현재 보드에 생성된 지뢰 개수
+
+	int isAlive = 1;
 	
 	srand((unsigned int)time(NULL));
 
-	//보드에 지뢰 생성
-	//보드 -1: 확인, 0: 지뢰X, 1: 지뢰
+	//보드 생성
 	for (int i = 0; i < 10; ++i)
 	{
 		for (int j = 0; j < 10; ++j)
 		{
-			int randNum = rand() % 5;
-			if (randNum == 1) board[j][i] = 1;		//20으로 나눈 나머지 값이 1이면 지뢰
-			else board[j][i] = 0;					//그 외에는 지뢰 X
+			int randNum = rand() % 8;
+			if (randNum == 1) board[i][j] = '*';		//20으로 나눈 나머지 값이 1이면 지뢰ㅇㅇ
+			else board[i][j] = '0';
+		}
+	}
+
+	//지뢰에 맞춰서 보드 숫자 갱신
+	for (int i = 0; i < 10; ++i)
+	{
+		for (int j = 0; j < 10; ++j)
+		{
+			if (board[i][j] != '*')
+			{
+				int mineNum = 0;
+				int x, y, xMax, yMax;
+				if (j == 0) x = 0; else x = -1;
+				if (i == 0) y = 0; else y = -1;
+				if (j == 9) xMax = 1; else xMax = 2;
+				if (i == 9) yMax = 1; else yMax = 2;
+
+				for (int h = y; h < yMax; h++)
+				{
+					for (int w = x; w < xMax; w++)
+					{
+						if (board[i + h][j + w] == '*') ++mineNum;
+					}
+				}
+
+				board[i][j] = '0' + mineNum;
+			}
 		}
 	}
 
 	//보드 확인용 출력
-	/*
 	for (int i = 0; i < 10; ++i)
 	{
 		for (int j = 0; j < 10; ++j)
 		{
-			printf("%2d", board[j][i]);					//그 외에는 지뢰 X
+			printf("%c", board[i][j]);					//그 외에는 지뢰 X
 		}
 		printf("\n");
 	}
-	*/
 
+	
+	//스크린 초기화
+	for (int i = 0; i < 10; ++i)
+	{
+		for (int j = 0; j < 10; ++j)
+		{
+			screen[i][j] = '+';	//스크린을 초기화
+		}
+	}
 
+	for (int i = 0; i < 10; ++i)
+	{
+		for (int j = 0; j < 10; ++j)
+		{
+			printf("%c", screen[i][j]);					//그 외에는 지뢰 X
+		}
+		printf("\n");
+	}
+
+	// 지뢰찾기 게임 순서
+	// 0. 초기화 및 기본 화면 출력
+	// ======여기부터 반복======
+	// 1. 플레이어 입력
+	// 2. 해당 좌표와 보드 확인
+	// ------지뢰일 경우------
+	// 3. 보드를 전부 밝히고 게임 종료
+	// ------지뢰가 아닐 경우------
+	// 3. 주변에 지뢰가 0일 경우 주변 쫙 확인
+	// 3-1. 숫자일 경우 해당 칸만 확인
+	// ==========================
+
+	while (isAlive)	//살아있으면 계속 실행	
+	{
+		printf("\n\n\n");
+		int inputX = -1, inputY = -1;
+		printf("X Y좌표 입력(띄어쓰기로 구분, 1~10): ");
+		scanf(" %d %d", &inputX, &inputY);
+
+		isAlive = Dig(inputX, inputY, board, screen);
+		for (int i = 0; i < 10; ++i)
+		{
+			for (int j = 0; j < 10; ++j)
+			{
+				printf("%c", screen[i][j]);					//그 외에는 지뢰 X
+			}
+			printf("\n");
+		}
+
+	}
+	
 
 	return 0;
 }
